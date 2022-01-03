3c4fc3823e933731ec9597ade73c505cb8e4ea98
Siddharth Bhatnagar <siddharthb1604@gmail.com>
2022-01-03 20:56:59 +0530

    added the repo link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
+      <a href="https://github.com/htrahddis-hub/keeper-app">Github</a>
       <p>Copyright ⓒ {year}</p>
     </footer>
   );
